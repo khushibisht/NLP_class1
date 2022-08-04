73f51f7254d5baf104a7e6aba4c014f32256d9e0
khushibisht <83939484+khushibisht@users.noreply.github.com>
2022-08-04 09:23:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 import pickle
 import numpy as np
-import nltk
 import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
